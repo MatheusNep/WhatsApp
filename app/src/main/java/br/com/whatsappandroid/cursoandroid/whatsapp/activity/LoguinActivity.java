05767b909bbe8f663b4ea21d1922e295cec7fe00
MatheusNep <matheussaboia@hotmail.com>
2019-01-29 11:38:26 -0200

    Implementado a classe de permissões para a utilizaão do SMS

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoguinActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoguinActivity.java
@@ -1,6 +1,10 @@
 package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -17,6 +21,7 @@ import java.util.Random;
 import java.util.StringTokenizer;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.helper.Permissao;
 import br.com.whatsappandroid.cursoandroid.whatsapp.helper.Preferencias;
 
 public class LoguinActivity extends AppCompatActivity {
@@ -25,6 +30,10 @@ public class LoguinActivity extends AppCompatActivity {
     private EditText ddd;
     private EditText ddi;
     private Button cadastrar;
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.SEND_SMS
+
+    };
 
 
 
@@ -32,6 +41,8 @@ public class LoguinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loguin);
+
+        Permissao.validaPermissao(1, this, permissoesNecessarias);
 
         nome      = (EditText) findViewById(R.id.nomeId);
         telefone  = (EditText) findViewById(R.id.idEdit_telefone);
@@ -99,5 +110,30 @@ public class LoguinActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permission, int[] grantResult){
+        super.onRequestPermissionsResult(requestCode,permission,grantResult);
+        for (int resultado : grantResult){
+            if (resultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar esse app é necessário aceitar as permissões");
+
+        builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
