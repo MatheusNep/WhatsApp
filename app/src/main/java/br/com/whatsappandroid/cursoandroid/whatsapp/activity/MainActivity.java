c56919bdd153f89604912114d519693c0955f376
MatheusNep <matheussaboia@hotmail.com>
2019-01-30 16:50:57 -0200

    Implementado Toolbar na tela principal

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,13 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     private Button botaoSair;
     private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = (Button) findViewById(R.id.botao_sair);
+        /*botaoSair = (Button) findViewById(R.id.botao_sair);
 
         botaoSair.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,10 +45,18 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Sucesso ao fazer logout", Toast.LENGTH_LONG).show();
 
             }
-        });
+        });*/
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Whatsapp");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+        
+    }
 
-
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
