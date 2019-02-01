ddb85f9503919d4bdac12fdeb72c15e03e83068c
MatheusNep <matheussaboia@hotmail.com>
2019-02-01 16:40:34 -0200

    Implementado duas tabs para conversas e contados, utilizando as classes SLidingTabLayout e SlidingTabStrip

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,13 +19,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.adapter.TabAdapter;
 import br.com.whatsappandroid.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
+import br.com.whatsappandroid.cursoandroid.whatsapp.helper.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button botaoSair;
     private FirebaseAuth usuarioAutenticacao;
     private Toolbar toolbar;
+
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
 
 
 
@@ -33,25 +40,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAuth();
-
-        /*botaoSair = (Button) findViewById(R.id.botao_sair);
-
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
-                autenticacao.signOut();
-
-
-                Toast.makeText(MainActivity.this, "Sucesso ao fazer logout", Toast.LENGTH_LONG).show();
-
-            }
-        });*/
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Whatsapp");
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
+
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.stl_tab);
+        viewPager = (ViewPager) findViewById(R.id.vp_pagina);
+
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this,R.color.colorAccent));
+
+        TabAdapter tabAdapter = new TabAdapter( getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        slidingTabLayout.setViewPager(viewPager);
 
     }
 
---
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/helper/Base64Custon.java
@@ -0,0 +1,4 @@
+package br.com.whatsappandroid.cursoandroid.whatsapp.helper;
+
+public class Base64Custon {
+}
